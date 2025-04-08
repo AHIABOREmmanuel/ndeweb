e52036fca1237059a47419d8b45938a932093bbc
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-04-08 23:18:06 +0000

    deploiement sur vercel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
