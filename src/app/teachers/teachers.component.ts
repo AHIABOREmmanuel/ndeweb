b399501438ec9e5198faa8f9fc0cf048756692be
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-04-10 11:43:31 +0000

    10/04/2025

--- src/app/teachers/teachers.component.ts
+++ src/app/teachers/teachers.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-teachers',
   templateUrl: './teachers.component.html',
-  host: {ngSkipHydration : 'false'},
   styleUrl: './teachers.component.css'
 })
 export class TeachersComponent {
--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-wrapper',
   templateUrl: './wrapper.component.html',
-  host: {ngSkipHydration : 'false'},
   styleUrls: ['./wrapper.component.css']
 })
 export class WrapperComponent {
