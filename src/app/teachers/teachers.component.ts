c53d282ff27176b76600b47ff8883271816bc637
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-04-10 10:13:12 +0000

    ngSkipHydration

--- src/app/teachers/teachers.component.ts
+++ src/app/teachers/teachers.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-teachers',
   templateUrl: './teachers.component.html',
+  host: {ngSkipHydration : 'false'},
   styleUrl: './teachers.component.css'
 })
 export class TeachersComponent {
--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -1,42 +1,11 @@
-import { Component, ElementRef, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-
-declare var require: any; // Permet d'utiliser require() dans TypeScript
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-wrapper',
   templateUrl: './wrapper.component.html',
+  host: {ngSkipHydration : 'false'},
   styleUrls: ['./wrapper.component.css']
 })
-export class WrapperComponent implements AfterViewInit {
+export class WrapperComponent {
 
-  constructor(
-    private el: ElementRef,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
-
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.initCarousel();
-    }
-  }
-
-  private initCarousel() {
-    // Charger jQuery et Owl Carousel à la volée
-    const $ = require('jquery');
-    require('owl.carousel');
-
-    $(this.el.nativeElement).find('.owl-carousel').owlCarousel({
-      items: 3,
-      loop: true,
-      nav: true,
-      dots: true,
-      autoplay: true,
-      responsive: {
-        0: { items: 1 },
-        768: { items: 2 },
-        992: { items: 3 }
-      }
-    });
-  }
 }
